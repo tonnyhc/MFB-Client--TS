895c37a0ad63de4f1d79c2ddfa83a8368a1ef7da
Toni <smokercho56@gmail.com>
2023-11-13 17:27:39 +0200

    Created an Authentication Context

---
+++ my-fit-buddy/src/contexts/AuthContext.tsx
@@ -0,0 +1,47 @@
+import { ReactNode, useState } from "react";
+import { createContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+type AuthData = {
+  token?: string;
+  username?:string,
+
+};
+
+type AuthContextType = {
+  authData: AuthData;
+  isAuth: boolean;
+  userLogin: (authData: AuthData) => void;
+  userLogout: () => void;
+};
+
+type AuthProviderProps = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
+
+export const AuthProvider:React.FC<AuthProviderProps> = ({ children }) => {
+  const [authData, setAuthData] = useLocalStorage<AuthData>("userData", {});
+
+  function userLogin(authData: AuthData): void {
+    setAuthData(authData);
+  }
+
+  function userLogout(): void {
+    setAuthData({});
+  }
+
+  const context = {
+    authData,
+    isAuth: authData.token ? true : false,
+    userLogin,
+    userLogout,
+  };
+
+  return (
+    <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
+  );
+};
